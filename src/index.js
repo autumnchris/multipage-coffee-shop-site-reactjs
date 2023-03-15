75cc82d9ac0cdb45641216e820c051b0ca72ea54
autumnchris <autumncbullard@gmail.com>
2023-03-15 13:45:09 -0400

    Add favicon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom"
 import App from './App';
 import 'file-loader?name=[name].[ext]!./index.html';
+import 'file-loader?name=[name].[ext]!./favicon.ico';
 import 'normalize.css';
 import './stylesheets/style.scss';
 
