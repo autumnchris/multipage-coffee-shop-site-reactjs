f3658b402b4549b12be03c084aed33f8b7700f68
autumnchris <autumncbullard@gmail.com>
2023-03-20 21:33:59 -0400

    Add a Back to Home button on the 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <section className="main-content not-found">
       <h2>404: Page not found</h2>
+      <Link to="/" className="button">Back to Home</Link>
     </section>
   );
 }
