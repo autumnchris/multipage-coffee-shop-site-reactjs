b1ee1f0eb7269787a78d60a8ae2aa81c19d2812a
autumnchris <autumncbullard@gmail.com>
2023-04-17 13:52:35 -0400

    Use single quotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Menu from './pages/Menu';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'file-loader?name=[name].[ext]!./index.html';
 import 'file-loader?name=[name].[ext]!./favicon.ico';
