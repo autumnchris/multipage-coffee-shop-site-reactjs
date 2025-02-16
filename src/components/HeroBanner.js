d932050e0de8d89f4bf30242d2dfc6010f3cccb2
autumnchris <autumncbullard@gmail.com>
2025-02-16 14:52:47 -0500

    Improve semantic HTML

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const HeroBanner = ({ heading, imageFile }) => {
   return (
-    <section className="hero-banner">
+    <div className="hero-banner">
       <div className="hero-banner-image" style={{ backgroundImage: `url(${imageFile})` }}></div>
       <div className="hero-banner-content">
         <h2>{heading}</h2>
       </div>
-    </section>
+    </div>
   );
 }
 
